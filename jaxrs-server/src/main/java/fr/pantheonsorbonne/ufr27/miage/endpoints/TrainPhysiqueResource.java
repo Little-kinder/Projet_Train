0ae2b714cdcf3c06d824a5c0e9419cb1a499beae
Paul Piotrowski <paul.piotro@gmail.com>
2020-10-22 12:50:40 +0200

    Resource

--- jaxrs-server/src/main/java/fr/pantheonsorbonne/ufr27/miage/endpoints/TrainPhysiqueResource.java
+++ jaxrs-server/src/main/java/fr/pantheonsorbonne/ufr27/miage/endpoints/TrainPhysiqueResource.java
@@ -1,5 +1,6 @@
 package fr.pantheonsorbonne.ufr27.miage.endpoints;
+import fr.pantheonsorbonne.ufr27.miage.TrainPhysique;
 
-public class TrainphysiqueResource {
+public class TrainPhysiqueResource {
 	
 }
